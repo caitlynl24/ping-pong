6fd0ed28659b6116909e141b9c4543e8bcfb358c
Caitlyn Lefebvre <clefebvre@rollins.edu>
2025-09-10 10:04:22 -0400

    paddle scripts
    Wrote the scripts for each paddle

--- ping-pong/Assets/Scripts/left_paddle_controller.cs
+++ ping-pong/Assets/Scripts/left_paddle_controller.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class left_paddle_controller : MonoBehaviour
 {
+
+    Rigidbody2D pad;
+    Vector2 initial;
+    public float displacement;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        pad = GetComponent<Rigidbody2D>();
+        initial = pad.transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if((Input.GetKey(KeyCode.Q)))
+        {
+            if(initial.y <= 3.6)
+            {
+                initial.y = initial.y + displacement;
+            }
+        }
+
+        else if((Input.GetKey(KeyCode.A)))
+        {
+            if(initial.y >= -3.6)
+            {
+                initial.y = initial.y - displacement;
+            }
+        }
+
+        pad.MovePosition(initial);
     }
 }
--- ping-pong/Assets/Scripts/right_paddle_controller.cs
+++ ping-pong/Assets/Scripts/right_paddle_controller.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class right_paddle_controller : MonoBehaviour
 {
+    Rigidbody2D pad;
+    Vector2 initial;
+    public float displacement;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        pad = GetComponent<Rigidbody2D>();
+        initial = pad.transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if((Input.GetKey(KeyCode.P)))
+        {
+            if(initial.y <= 3.6)
+            {
+                initial.y = initial.y + displacement;
+            }
+        }
+
+        else if((Input.GetKey(KeyCode.L)))
+        {
+            if(initial.y >= -3.6)
+            {
+                initial.y = initial.y - displacement;
+            }
+        }
+
+        pad.MovePosition(initial);
     }
 }
