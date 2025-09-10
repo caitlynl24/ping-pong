6770633b2d812f5a3743626b9b46781f00c17cc5
Caitlyn Lefebvre <clefebvre@rollins.edu>
2025-09-10 10:23:03 -0400

    added ball & script
    Added a ball sprite and a script for the ball and it moves, just not in a straight line

---
+++ ping-pong/Assets/Scripts/ball.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Rigidbody2D rb;
+    public float speed;
+    public Vector2 direction;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //(1, 1)
+        direction = Vector2.one.normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = direction * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("left_paddle"))
+        {
+            direction.x = -direction.x;
+        }
+
+        else if(collision.gameObject.CompareTag("right_paddle"))
+        {
+            direction.x = -direction.x;
+        }
+    }
+}
